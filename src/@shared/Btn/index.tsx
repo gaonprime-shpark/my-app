1261aded4daf1eedf291a2b73df66206f4c75bff
sung hyun park <shpark@gaonprime.com>
2023-04-17 10:31:57 +0900

    feat: prac commit

--- src/@shared/Btn/index.tsx
+++ src/@shared/Btn/index.tsx
@@ -16,4 +16,16 @@ const Btn = ({ OnClick, text }: BtnInterface) => {
   );
 };
 
+//test 브랜치에 병합하기
+const BtnTest = ({ OnClick, text }: BtnInterface) => {
+  return (
+    <button
+      className="p-4 m-2 text-white bg-green-600 rounded-md hover:bg-green-950"
+      onClick={OnClick}
+    >
+      {text}
+    </button>
+  );
+};
+
 export default React.memo(Btn);
