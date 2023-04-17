43ee9ed843b2ac95021184519c82601524fc9656
sung hyun park <shpark@gaonprime.com>
2023-04-17 10:33:28 +0900

    feat: commit prac

--- src/@shared/Btn/index.tsx
+++ src/@shared/Btn/index.tsx
@@ -17,7 +17,19 @@ const Btn = ({ OnClick, text }: BtnInterface) => {
 };
 
 //test 브랜치에 병합하기
+//git rebase master
 const BtnTest = ({ OnClick, text }: BtnInterface) => {
+  return (
+    <button
+      className="p-4 m-2 text-white bg-green-600 rounded-md hover:bg-green-950"
+      onClick={OnClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+const BtnPrac = ({ OnClick, text }: BtnInterface) => {
   return (
     <button
       className="p-4 m-2 text-white bg-green-600 rounded-md hover:bg-green-950"
